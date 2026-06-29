ffe3b7fdb4597a35b7ac6030f2088be86c63574a
chukwuemekanweke <emekanweke604@gmail.com>
2026-06-29 01:29:41 +0100

    Readme update

--- docker-compose.env.example.sh
+++ docker-compose.env.example.sh
@@ -6,3 +6,5 @@ export MAILTRAP_TOKEN=""
 export MAILTRAP_WEBHOOK_SIGNING_SECRET=""
 export MAILTRAP_FROM_ADDRESS=""
 export MAILTRAP_FROM_NAME="BackendProjectTemplate"
+
+docker compose up -d --build --force-recreate
