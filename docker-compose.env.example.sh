85b29488e0f764aeadbcff01623e4e96dc8341b6
chukwuemekanweke <emekanweke604@gmail.com>
2026-06-29 01:16:13 +0100

    Minor update to address first user complaint

--- docker-compose.env.example.sh
+++ docker-compose.env.example.sh
@@ -3,3 +3,6 @@
 export GITHUB_USERNAME=""
 export GITHUB_PAT=""
 export MAILTRAP_TOKEN=""
+export MAILTRAP_WEBHOOK_SIGNING_SECRET=""
+export MAILTRAP_FROM_ADDRESS=""
+export MAILTRAP_FROM_NAME="BackendProjectTemplate"
