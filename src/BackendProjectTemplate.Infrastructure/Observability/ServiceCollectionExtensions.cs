09b61d698a3f4ce78dfbc697ce1ed94ed9152c46
chukwuemekanweke <emekanweke604@gmail.com>
2026-09-21 07:36:05 +0100

    Fix compile error

--- src/BackendProjectTemplate.Infrastructure/Observability/ServiceCollectionExtensions.cs
+++ src/BackendProjectTemplate.Infrastructure/Observability/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 using BackendProjectTemplate.Domain.Common.Observability;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
